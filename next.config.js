eabe2695c554c1c3ac4c10d9acac7dcb63976c9a
Muhammad Rendi <muhammad.rendi8@gmail.com>
2022-02-03 16:42:03 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI: "mongodb+srv://root:root@crudsynapsis.a1v71.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  }
+  reactStrictMode: true
 }
